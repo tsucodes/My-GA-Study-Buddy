c203c27c2c3f0161fbe627f3d2349018f62f5481
Tsani Rhodes <rhodestsani@gmail.com>
2022-03-28 16:57:47 -0400

    js started

---
+++ study-buddy.js
@@ -0,0 +1,21 @@
+// define all DOM elements that I need to target
+// create event listener for container-card to flip when clicked
+// 
+
+
+
+// define Dom elements to manipulate
+let card = document.querySelector(".container-cards #flip")
+let question = document.querySelector(".question-side")
+let answer = document.querySelector(".answer-side")
+let correctBtn = document.querySelector(".correct")
+let incorrectBtn = document.querySelector(".incorrect")
+let nextBtn = document.querySelector(".next")
+let resetBtn = document.querySelector(".reset")
+
+// questions stored in an array of objects 
+
+// functions
+//  for inital card
+
+// event listeners
