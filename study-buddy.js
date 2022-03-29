981056c228f61e934787bd523d1ca6dc2abc64aa
Tsani Rhodes <rhodestsani@gmail.com>
2022-03-29 09:48:12 -0400

    html elements targeted in css file

--- study-buddy.js
+++ study-buddy.js
@@ -1,13 +1,16 @@
 // define all DOM elements that I need to target
-// create event listener for container-card to flip when clicked
+// create event listener for container-card to hide answer but display when clicked
+// create an event listener for container-card to flip when clicked
+// create an array of objects that hold Q's & A's
+// how will the click event listener access index of Q &A?
 // 
 
 
 
 // define Dom elements to manipulate
 let card = document.querySelector(".container-cards #flip")
-let question = document.querySelector(".question-side")
-let answer = document.querySelector(".answer-side")
+let question = document.querySelector(".question-card")
+let answer = document.querySelector(".answer-card")
 let correctBtn = document.querySelector(".correct")
 let incorrectBtn = document.querySelector(".incorrect")
 let nextBtn = document.querySelector(".next")
